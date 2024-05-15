42a059c8021ca8d4052f6cb399222775cb60a060
pb1n <107804218+pb1n@users.noreply.github.com>
2024-05-15 03:53:10 +0100

    Code now displays PNGS

--- NodeCode/src/main.cpp
+++ NodeCode/src/main.cpp
@@ -1,5 +1,9 @@
 #include <Adafruit_NeoPixel.h>
 #include <random>
+#include <lodepng.h>
+#include "FS.h"
+#include "SPIFFS.h"
+
 #define PIN 16
 #define NUMPIXELS 56
 #define DIMMING_FACTOR 16
@@ -10,26 +14,23 @@
 #define GRID_WIDTH 8
 #endif
 
-#define RED 0xFF0000
-#define GREEN 0x00FF00
-#define BLUE 0x0000FF
-#define WHITE 0xFFFFFF
+#define COLOUR_RED 0xFF0000
+#define COLOUR_GREEN 0x00FF00
+#define COLOUR_BLUE 0x0000FF
+#define COLOUR_WHITE 0xFFFFFF
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-#define DELAYVAL 40
-
-int dimmedColour(int colour)
-// Dims a Hex colour code by a given factor
-{
-  return ((colour >> 16) / DIMMING_FACTOR << 16) |
-         ((colour >> 8 & 0xFF) / DIMMING_FACTOR << 8) |
-         ((colour & 0xFF) / DIMMING_FACTOR);
-}
+#define DELAYVAL 500
 
 void setPixel(int pixel, int colour)
-// Sets a pixel to a given colour
+// Sets a pixel to a given colour.
 {
-  pixels.setPixelColor(pixel, dimmedColour(colour));
+  pixels.setPixelColor(pixel, colour);
+}
+
+void updatePixels()
+// Updates pixels. Use this to show the next 'frame' in an animation.
+{
   pixels.show();
 }
 
@@ -65,11 +66,69 @@ void drawLineStraight(int xy, bool vertical = true, int colour = 0xFFFFFF)
   }
 }
 
+void drawImage(char *filepath)
+// Displays an image on the grid.
+{
+  std::vector<unsigned char> image; // Raw pixel data
+  unsigned width, height;
+
+  // Open file from SPIFFS
+  File file = SPIFFS.open(filepath, "r");
+  if (!file)
+  {
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+
+  // Read file contents into buffer
+  std::vector<unsigned char> png_data;
+  while (file.available())
+  {
+    png_data.push_back(file.read());
+  }
+  file.close();
+
+  // Decode PNG
+  unsigned error = lodepng::decode(image, width, height, png_data);
+
+  if (error)
+  {
+    Serial.print("Decoder error: ");
+    Serial.println(error);
+    return;
+  }
+
+  // Ensuring the image dimensions match the LED grid
+  if (width != GRID_WIDTH || height != GRID_HEIGHT)
+  {
+    Serial.println("Image dimensions do not match the LED grid size.");
+    return;
+  }
+
+  // Handling different color types
+  for (int y = 0; y < height; y++)
+  {
+    for (int x = 0; x < width; x++)
+    {
+      int ledIndex = gridToSeries(x, y);    // Map 2D (x, y) to 1D index
+      int pixelIndex = 4 * (y * width + x); // Each pixel has 4 components (RGBA)
+      uint8_t r = image[pixelIndex];
+      uint8_t g = image[pixelIndex + 1];
+      uint8_t b = image[pixelIndex + 2];
+      uint8_t a = image[pixelIndex + 3];
+
+      // Apply alpha transparency
+      r = (r * a) / 255;
+      g = (g * a) / 255;
+      b = (b * a) / 255;
+
+      setPixel(ledIndex, pixels.Color(r, g, b));
+    }
+  }
+  updatePixels();
+}
+
 // void drawLine(int x1, int y1, int x2, int y2){
-
-// }
-
-// void loadBMP(){
 
 // }
 
@@ -92,26 +151,45 @@ void drawLineStraight(int xy, bool vertical = true, int colour = 0xFFFFFF)
 void setup()
 {
   Serial.begin(115200);
+  pixels.setBrightness(4);
   pixels.begin();
+
+  if (!SPIFFS.begin(true))
+  {
+    Serial.println("An error has occurred while mounting SPIFFS");
+    return;
+  }
 }
 
 void loop()
 {
-  pixels.clear();
-  drawLineStraight(2, true, BLUE);
-  drawLineStraight(5, true, BLUE);
-  delay(2000);
-  pixels.clear();
-  drawLineStraight(1, false, GREEN);
-  drawLineStraight(5, false, GREEN);
-  delay(2000);
-
-  // drawCircle(7, 8);
-  //  for (int i = 0; i < NUMPIXELS; i++)
-  //  {
-  //    setPixel(i, 0xFFFFFF);
-  //    delay(DELAYVAL);
-  //  }
-  // setPixel(gridToSeries(4, 3), 0xFFFFFF);
+  // bool redFirst;
+  // for (int i = 0; i < 4; i++)
+  // {
+  //   drawLineStraight(2 * i, true, COLOUR_WHITE);
+  //   drawLineStraight(2 * i + 1, true, COLOUR_BLUE);
+  // }
+  // updatePixels();
   // delay(DELAYVAL);
+
+  // for (int i = 0; i < 4; i++)
+  // {
+  //   drawLineStraight(2 * i, true, COLOUR_BLUE);
+  //   drawLineStraight(2 * i + 1, true, COLOUR_WHITE);
+  // }
+  // updatePixels();
+  // delay(DELAYVAL);
+  // drawImage("images/test2.png");
+  drawImage("/colourBars.png");
+  updatePixels();
+  delay(500);
+  drawImage("/kirby.png");
+  updatePixels();
+  delay(25000);
+  drawImage("/colourBars.png");
+  updatePixels();
+  delay(500);
+  drawImage("/test2.png");
+  updatePixels();
+  delay(2500);
 }
